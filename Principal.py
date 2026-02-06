edf2ba7bd83405a353f00b0e41a836889ea622c3
keinerjaramillo28-dot <keinerjaramillo28@gmail.com>
2026-02-06 06:58:38 -0500

    Implement employee database class with methods

---
+++ Principal.py
@@ -0,0 +1,43 @@
+class base_datos_empleados:
+  def _init_(self):
+    #ARRAY
+    self.db_empleado_lista=[]
+
+def agregra_empleados(self,objeto_empleado):
+  self.db_empleado_lista.appen(obj_empleado)
+  return true
+
+def  guarar_varios_empleados(self,varios_obj):
+  self. db_empleado_lista.extend(varios_obj)
+
+def imprimir_info(self):
+  for i in 
+range(len(self.db_empleado_lista)):
+
+  print(self.db_empleado_lista[i].ver_info_empleado())
+
+def inserta_empleados(self,posicion, obj_empleado):
+  self.db_empleado_lista.insert(posicion, obj_empleado)
+  return true
+
+def eliminar_ult_lista(self):
+  self.db_empleado_lista.pop()
+  return true
+
+def eliminar_empleado_por_posicion(self, posicion):
+  self.db_empleado_lista.remove(self.db_empleado_lista([posicion])
+    return true
+
+def obtener_nombre(self,empleado):
+  return
+  empleado.get_nombre_empleado()
+
+def sort_empleados(self):
+  self.db_empleado_lista.sort(key=self.obtener_nombre)
+  return true 
+
+def reverse_empleados(self):
+  self.db_empleado_lista.reverse()
+  return true
+  
+  
